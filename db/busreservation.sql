d68a2bf33acda9975cb8175b2f3dc0c390cb2dad
Glency Tirao <glency.dagusoft@gmail.com>
2019-10-16 11:38:49 +0800

    Update Database

--- db/busreservation.sql
+++ db/busreservation.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jul 01, 2019 at 04:10 AM
--- Server version: 10.1.29-MariaDB
--- PHP Version: 7.2.0
+-- Host: localhost
+-- Generation Time: Oct 16, 2019 at 05:37 AM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -34,14 +34,6 @@ CREATE TABLE `obr_buses` (
   `status` varchar(10) DEFAULT 'available'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `obr_buses`
---
-
-INSERT INTO `obr_buses` (`id`, `bus_name`, `status`) VALUES
-(1, 'Lorem Ipsum', 'available'),
-(2, 'Bus Name', 'available');
-
 -- --------------------------------------------------------
 
 --
@@ -55,18 +47,23 @@ CREATE TABLE `obr_requests` (
   `contact` varchar(13) NOT NULL,
   `email` varchar(100) NOT NULL,
   `address` text NOT NULL,
+  `company` varchar(500) NOT NULL,
   `purpose` text NOT NULL,
   `remarks` text NOT NULL,
   `status` varchar(10) DEFAULT 'pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `obr_requests`
+-- Table structure for table `obr_schedules`
 --
 
-INSERT INTO `obr_requests` (`id`, `fname`, `lname`, `contact`, `email`, `address`, `purpose`, `remarks`, `status`) VALUES
-(1, 'Juan', 'Dela Cruz', '09672105875', 'darko05.gt@gmail.com', 'darko05.gt@gmail.com', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Id exercitationem, voluptatem accusantium, inventore nesciunt suscipit error ipsum expedita deleniti tenetur adipisci nam, fuga unde qui quisquam ipsam quos consequatur! Sed!', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Id exercitationem, voluptatem accusantium, inventore nesciunt suscipit error ipsum expedita deleniti tenetur adipisci nam, fuga unde qui quisquam ipsam quos consequatur! Sed!', 'pending'),
-(2, 'Juan', 'Dela Cruz', '09999999999', 'juandelacruz@gmail.com', 'Lingayen, Pangasinan', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores officia reiciendis, optio magni accusantium earum eaque provident asperiores dignissimos, impedit dolores natus animi esse quos blanditiis explicabo accusamus corrupti minus.', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores officia reiciendis, optio magni accusantium earum eaque provident asperiores dignissimos, impedit dolores natus animi esse quos blanditiis explicabo accusamus corrupti minus.', 'pending');
+CREATE TABLE `obr_schedules` (
+  `id` int(255) NOT NULL,
+  `bus_id` int(11) NOT NULL,
+  `reserve_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -86,8 +83,8 @@ CREATE TABLE `obr_users` (
 --
 
 INSERT INTO `obr_users` (`id`, `username`, `pw`, `role`) VALUES
-(1, 'pgo', '1618cdbd6e3c266ee2406d77e3601eb0', 'pgo'),
-(2, 'engineer', '1618cdbd6e3c266ee2406d77e3601eb0', 'engineer');
+(1, 'pgo', 'e87632157a7ea2f03deb124b2f2e2219', 'pgo'),
+(2, 'engineer', 'e87632157a7ea2f03deb124b2f2e2219', 'engineer');
 
 --
 -- Indexes for dumped tables
@@ -106,6 +103,12 @@ ALTER TABLE `obr_requests`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `obr_schedules`
+--
+ALTER TABLE `obr_schedules`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `obr_users`
 --
 ALTER TABLE `obr_users`
@@ -119,13 +122,19 @@ ALTER TABLE `obr_users`
 -- AUTO_INCREMENT for table `obr_buses`
 --
 ALTER TABLE `obr_buses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `obr_requests`
 --
 ALTER TABLE `obr_requests`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `obr_schedules`
+--
+ALTER TABLE `obr_schedules`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `obr_users`
